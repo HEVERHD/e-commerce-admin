b119d0e3d36386a0881f3ba33ecb2bc5738981ad
Everyone's store <101605474+E-commerce-pf@users.noreply.github.com>
2022-03-22 13:09:31 -0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux'
 import { getAllProducts } from "../../redux/actions";
-import { useEffect } from "react";
-import './Home.modules.css';
+import style from './Home.module.scss';
 
 //COMPONENTES
 import Sidebar from "../../components/SideBar/Sidebar";
@@ -14,7 +14,10 @@ import ProductContainer from './productContainer/ProductContainer';
       Opcion de crear más productos
 */
 
+const pageSize = 10 // Para cambiar el tamaño del paginado
+
 const Home = ()=>{
+      const [page, setPage] = useState(0);
       const dispatch = useDispatch();
       const token = useSelector( state => state.currentUser.accessToken)
       //Buscamos todos los productos creados
@@ -26,33 +29,48 @@ const Home = ()=>{
 
 
       return (
-            <div className='containerHome'>
+            <div className={style.container}>
                   <Sidebar/>
 
-                  <div className='productsContainer'>
+                  <div className={style.tablecontainer}>
 
                         <h1>Total productos : {products.length} </h1>
-                        <div className='categories'>
-                              <h1 className='txt_categorias'>TITULO</h1>
-                              <h1 className='txt_categorias'>PRECIO</h1>
-                              <h1 className='txt_categorias'>CANTIDAD</h1>
-                              <h1 className='txt_categorias'>VENDIDO</h1>
-                              <h1 className='txt_categorias'>DESCUENTO</h1>
+                        <div className={style.categories}>
+                              <h1>TITLE</h1>
+                              <h1>PRICE</h1>
+                              <h1>STOCK</h1>
+                              <h1>SALES</h1>
+                              <h1>DISCOUNT</h1>
+                        </div>
+                        <div className={style.productContainer}>
+                              {products.result?.slice(page*pageSize,(page+1)*pageSize).map((item,index)=>{
+                                    return <ProductContainer
+                                          id = {item.id} 
+                                          title = {item.title}
+                                          price = {item.price}
+                                          stock = {item.stock}
+                                          sales = {item.sales}
+                                          discount = {item.discount}
+                                          token={token}
+                                    />
+                              })}
                         </div>
 
-                        { products.result?.map( item =>{
-                              return <ProductContainer 
-                              id = {item.id} 
-                              title = {item.title}
-                              price = {item.price}
-                              stock = {item.stock}
-                              sales = {item.sales}
-                              discount = {item.discount}
-                              token={token}
-                              />
-                        })
-                        }
+                        <div className={style.pag}>
+                              <button type='button' onClick={()=>{
+                                    if(page>0) setPage(page-1);
+                              }}>
+                                    Previous
+                              </button>
+                              <button type='button' onClick={()=>{
+                                    if((page+1) * pageSize < products.length) setPage(page+1);
+                              }}>
+                                    Next
+                              </button>
+                        </div>
                   </div>
+
+
             </div>
       )
 }
