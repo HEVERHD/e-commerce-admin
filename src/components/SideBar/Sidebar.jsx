79dbdaeaad97f0693f7a795c878256784b639b61
BrunoAmerio <88524640+BrunoAmerio@users.noreply.github.com>
2022-03-22 13:44:03 -0300

    Actualice style

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import './Sidebar.modules.css'
+import style from './Sidebar.module.scss'
 
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material'
@@ -7,7 +7,7 @@ const Sidebar = ()=>{
       const navigate = useNavigate();
 
       return (
-            <div className='container_sideBar'>
+            <div className={style.container}>
                   <Button variant="outlined" className='btn_mui' onClick={()=> navigate('/home')}>Home</Button>
                   <Button variant="outlined" className='btn_mui' onClick={()=>navigate('/users')}>Users</Button>
                   <Button variant="outlined" className='btn_mui' onClick={()=>navigate('/transactions')}>Transactions</Button>
