9806716f15834ec6887f5cde25f2a29f166e9e9b
Everyone's store <101605474+E-commerce-pf@users.noreply.github.com>
2022-03-22 12:59:28 -0300

    Update Users.jsx

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,12 +1,24 @@
-import baseURL from "../../config/baseUrl";
-import Sidebar from '../../components/SideBar/Sidebar';
 import { useEffect, useState } from "react";
 import { deleteUser, getAllUsers } from "../../redux/actions";
 import { useDispatch, useSelector } from "react-redux";
 import style from './Users.module.scss';
+
+//COMPONENTES
 import { Link } from "react-router-dom";
+import Sidebar from '../../components/SideBar/Sidebar';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+import baseURL from "../../config/baseUrl";
+
+
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+
+
 /*
 Traer todos los usuarios 
 Mostrar los primeros 10 nuevos usuarios, y luego un boton para desplegar más 
@@ -16,80 +28,90 @@ Con un páginado de 20 por página
 const pageSize = 10 // Para cambiar el tamaño del paginado
 
 const Users = ()=>{
+      const [page, setPage] = useState(0);
+      const dispatch = useDispatch();
+      const users = useSelector(state=>state.allUsers);
+      const adminToken = useSelector(state=>state.currentUser.accessToken);
 
-      const [state, setState] = useState(false)
+      const toggleMenu = (id)=>{
+            console.log(document.querySelector(`#${id}`).classList)
+            document.querySelector(`#${id}`).classList.toggle(style.enable)
+      }
 
-      const [page, setPage] = useState(0)
-
-      const dispatch = useDispatch()
-
-      const users = useSelector(state=>state.allUsers)
-      const adminToken = useSelector(state=>state.currentUser.accessToken)
 
       useEffect(()=>{
             dispatch(getAllUsers(adminToken))
-      },[state, dispatch])
+      },[dispatch]);
 
-      console.log(users)
+      console.log(users);
       return(
             <div className={style.container}>
                   <Sidebar/>
-                  <h1>Users</h1>
-                  <table className={style.usersTable}>
-                        <tr>
-                              <th>Email</th>
-                              <th>Name</th>
-                              <th>Country</th>
-                              <th>Creation date</th>
-                              <th>User type</th>
-                              <th>Actions</th>
-                        </tr>
+                  
+                  <div className={style.tableContainer}>
+                        <h1>Users</h1>
+                        <div className={style.titleTable}>
+                              <h4>Email</h4>
+                              <h4>Name</h4>
+                              <h4>Country</h4>
+                              <h4>Creation date</h4>
+                              <h4>User type</h4>
+                        </div>
 
-                        {users.slice(page*pageSize,(page+1)*pageSize).map((e,i)=>{
-                        let date = new Date()
-                        let time = e.createdAt.split('T')[0].split('-').reverse()
-                        let yearsDiff = date.getFullYear()-time[2]
-                        let monthsDiff = date.getMonth()+1-time[1]
-                              return <tr className={style.selected}>
-                                    <td>{e.email}</td>
-                                    <td>{e.name+' '+e.lastName}</td>
-                                    <td>{e.country}</td>
-                                    <td>{time.join('/')}{yearsDiff+monthsDiff
-                                          ?', '+(yearsDiff?yearsDiff+' year'+(yearsDiff===1?'':'s'):'')+' '+(monthsDiff?monthsDiff+' month'+(monthsDiff===1?'':'s'):'')+' ago'
-                                          :''}</td>
-                                    <td>{e.isAdmin?'Admin':'Client'}</td>
-                                    <td className={style.options}>
-                                          <button className={style.edit}>
-                                                <Link to={`edit/${e.id}`} state={users[i]}>
-                                                      <span>Edit</span>
-                                                </Link>
-                                          </button>
+                        <div className={style.usersContainer}>
+                              {users.slice(page*pageSize,(page+1)*pageSize).map((e,i)=>{
+                                    let date = new Date();
+                                    let time = e.createdAt.split('T')[0].split('-').reverse();
+                                    let yearsDiff = date.getFullYear()-time[2];
+                                    let monthsDiff = date.getMonth()+1-time[1];
+                                    return <div className={style.userElement}>
+                                          <p>{e.email}</p>
+                                          <p>{e.lastName ? e.name +' '+e.lastName : e.name}</p>
+                                          <p>{e.country}</p>
+                                          <p>{time.join('/')}{yearsDiff+monthsDiff
+                                                ?', '+(yearsDiff?yearsDiff+' year'+(yearsDiff===1?'':'s'):'')+' '+(monthsDiff?monthsDiff+' month'+(monthsDiff===1?'':'s'):'')+' ago'
+                                                :''}</p>
+                                          <p>{e.isAdmin?'Admin':'Client'}</p>
 
-                                          <button className={style.erase} onClick={()=>{
-                                                dispatch(deleteUser(e.id, adminToken))
-                                                setState(!state)
-                                          }}>
-                                                <span>Delete</span>
-                                          </button>
-                                    </td>
-                              </tr>
-                        })}
+                                          <div className={style.menu}>
 
-                  </table>
-                  <div className={style.pag}>
-                        <button type='button' onClick={()=>{
-                              if(page>0) setPage(page-1)
-                        }}>
-                              Previous
-                        </button>
-                        <button type='button' onClick={()=>{
-                              if((page+1)*pageSize<users.length) setPage(page+1)
-                        }}>
-                              Next
-                        </button>
+                                                <button onClick={ ()=> toggleMenu('m'+e.name)}>
+                                                      <MoreVertIcon/>
+                                                </button>
+                                                <div className={style.menuContainer} id={`m${e.name}`}>
+                                                      <List >
+                                                            <ListItemButton component="button"  onClick={() => alert('Button')}>
+                                                                  <ListItemText primary='Forzar contraseña'/>
+                                                            </ListItemButton>
+                                                            <ListItemButton component="button" onClick={() => alert('Button')}>
+                                                                  <ListItemText primary='Cambiar privilegios'/>
+                                                            </ListItemButton>
+                                                            <ListItemButton component="button" onClick={() => alert('Button')} >
+                                                                  <ListItemText primary='Banear usuario' />
+                                                            </ListItemButton>
+                                                      </List>
+                                                </div>
+
+                                          </div>
+                                    </div>
+                                    
+                              })};
+
+                              <div className={style.pag}>
+                                    <button type='button' onClick={()=>{
+                                          if(page>0) setPage(page-1);
+                                    }}>
+                                          Previous
+                                    </button>
+                                    <button type='button' onClick={()=>{
+                                          if((page+1)*pageSize<users.length) setPage(page+1);
+                                    }}>
+                                          Next
+                                    </button>
+                              </div>
+                        </div>
                   </div>
             </div>
       )
 }
 export default Users;
-// {new Date().getFullYear()-time.split('/')[0]}{new Date().getMonth()}
