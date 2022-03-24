2a1bf21da1f712a40dc84bf89fe16af3a92a69ab
HEVERHD <hever11.hdgd@gmail.com>
2022-03-24 09:57:50 -0500

    desingDash

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -1,17 +1,35 @@
-import style from './Sidebar.module.scss'
+import style from './Sidebar.module.scss';
 
 import { useNavigate } from 'react-router-dom';
-import { Button } from '@mui/material'
+import { Button } from '@mui/material';
 
-const Sidebar = ()=>{
-      const navigate = useNavigate();
+const Sidebar = () => {
+	const navigate = useNavigate();
 
-      return (
-            <div className={style.container}>
-                  <Button variant="outlined" className='btn_mui' onClick={()=> navigate('/home')}>Home</Button>
-                  <Button variant="outlined" className='btn_mui' onClick={()=>navigate('/users')}>Users</Button>
-                  <Button variant="outlined" className='btn_mui' onClick={()=>navigate('/transactions')}>Transactions</Button>
-            </div>
-      )
-}
+	return (
+		<div className={style.container}>
+			<Button
+				variant='outlined'
+				className='btn_mui'
+				onClick={() => navigate('/home')}
+			>
+				Home
+			</Button>
+			<Button
+				variant='outlined'
+				className='btn_mui'
+				onClick={() => navigate('/users')}
+			>
+				Users
+			</Button>
+			<Button
+				variant='outlined'
+				className='btn_mui'
+				onClick={() => navigate('/transactions')}
+			>
+				Transactions
+			</Button>
+		</div>
+	);
+};
 export default Sidebar;
